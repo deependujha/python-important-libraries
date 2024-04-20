fd0cde224c23d515ab96fcdbca66014ce4a11645
Deependu Jha <deependujha21@gmail.com>
2024-04-20 09:33:47 +0530

    python-yaml-guide done

---
+++ 03-yaml-guide-python/main.py
@@ -0,0 +1,15 @@
+import yaml
+from pprint import pprint # pretty print
+
+with open("config.yaml") as f:
+    cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+
+pprint(cfg)
+pprint(cfg["pca_params"])
+
+cfg["n_components"] = 0.2
+cfg["umap_params"]["metrics"].append("chebyshev")
+
+with open("config.yaml", "w") as f:
+    cfg = yaml.dump(cfg, stream=f, default_flow_style=False, sort_keys=False)
